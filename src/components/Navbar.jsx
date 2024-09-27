7a0fe965c349d5ab55f79d2a3783526270c59c4e
shohbek <jalilovshohbek9909@gmail.com>
2024-09-27 17:58:11 +0500

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,40 @@ import { Link } from "react-router-dom";
 import { GlobalContext } from "../context/globalContext";
 // icon heart
 import { FaHeart } from "react-icons/fa";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { IoMdClose } from "react-icons/io";
+import { FcStackOfPhotos } from "react-icons/fc";
 
 // rrd
 function Navbar() {
   const { likeImageArr } = useContext(GlobalContext);
-  const [active, setActive] = useState(false);
   return (
     <header className="py-5 bg-slate-400">
-      <div className="flex justify-between items-center align-elements">
-        <Link className="text-white text-3xl">Logo</Link>
-        <nav className="hidden md:flex">
+      <div className="navbar flex justify-between items-center align-elements">
+        <div className="navbar-start">
+          <Link>
+            <FcStackOfPhotos className="text-4xl hidden md:flex" />
+          </Link>
+          <Link className="dropdown">
+            <FcStackOfPhotos className="text-4xl md:hidden" />
+            <ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+              <li>
+                <Link to="/" className="font-medium hover:text-black">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="font-medium hover:text-black">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="font-medium hover:text-black">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </Link>
+        </div>
+        <nav className="hidden md:flex navbar-center">
           <ul className="flex gap-5 items-center">
             <li>
               <Link
@@ -40,87 +62,17 @@ function Navbar() {
                 Contact
               </Link>
             </li>
-            <li>
-              <Link
-                to="/likes"
-                className="flex relative top-0 left-0 py-2 pr-3"
-              >
-                <FaHeart className="text-red-700 text-lg" />
-                <span className="absolute top-0 right-0 text-sm">
-                  {likeImageArr.length}
-                </span>
-              </Link>
-            </li>
           </ul>
         </nav>
-        <button
-          onClick={() => {
-            setActive(!active);
-          }}
-          className="text-white font-medium hover:text-slate-200 md:hidden"
-        >
-          {active ? (
-            <IoMdClose className="text-2xl" />
-          ) : (
-            <GiHamburgerMenu className="text-2xl" />
-          )}
+        <button className="text-white font-medium hover:text-slate-200 navbar-end">
+          <Link to="/likes" className="flex relative top-0 left-0 py-2 pr-3">
+            <FaHeart className="text-red-700 text-lg" />
+            <span className="absolute top-0 right-0 text-sm">
+              {likeImageArr.length}
+            </span>
+          </Link>
         </button>
       </div>
-      {active && (
-        <div className="modalContainer h-screen bg-slate-400 z-10 w-full fixed flex justify-center items-center">
-          <div className="menuModal w-full">
-            <ul className="flex flex-col gap-7 items-center">
-              <li>
-                <Link
-                  onClick={() => {
-                    setActive(false);
-                  }}
-                  to="/"
-                  className="text-black font-medium text-xl hover:text-slate-500"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => {
-                    setActive(false);
-                  }}
-                  to="/about"
-                  className="text-black font-medium text-xl hover:text-slate-500"
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => {
-                    setActive(false);
-                  }}
-                  to="/contact"
-                  className="text-black font-medium text-xl hover:text-slate-500"
-                >
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => {
-                    setActive(false);
-                  }}
-                  to="/likes"
-                  className="flex relative top-0 left-0 py-2 pr-3"
-                >
-                  <FaHeart className="text-red-700 text-2xl" />
-                  <span className="absolute top-0 right-0 text-sm">
-                    {likeImageArr.length}
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
     </header>
   );
 }
